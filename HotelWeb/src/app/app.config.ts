844ee71ef6b5937af7e97b38090b45a3a63b112b
hihihi28 <lynknhi288@gmail.com>
2024-11-04 17:00:15 +0700

    hi

--- HotelWeb/src/app/app.config.ts
+++ HotelWeb/src/app/app.config.ts
@@ -3,15 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { en_US, provideNzI18n, et_EE } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import et from '@angular/common/locales/et';
 
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+  providers: [provideRouter(routes), provideClientHydration(), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(), provideNzI18n(et_EE), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
 };
