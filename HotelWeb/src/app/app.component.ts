594a0e163d9a3d879996161ea2611d3fa8c12866
hihihi28 <lynknhi288@gmail.com>
2024-11-08 00:05:15 +0700

    oki

--- HotelWeb/src/app/app.component.ts
+++ HotelWeb/src/app/app.component.ts
@@ -1,35 +1,36 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
-@Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
-})
-export class RegisterComponent {
-  registerForm!: FormGroup;
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.registerForm = this.fb.group({
-      email: [null, [Validators.email, Validators.required]],
-      password: [null, Validators.required],
-      name: [null, Validators.required]
-    });
-  }
- 
-}
-
-
+import { UserStorageService } from './auth/services/storage/use-storage.service';
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // Your component logic here
+  title = 'HotelWeb';
+
+  isCustomerLoggedIn: boolean = UserStorageService.isCustomerLoggedIn();
+  isAdminLoggedIn: boolean = UserStorageService.isAdminLoggedIn();
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event.constructor.name === "NavigationEnd") {
+        this.isCustomerLoggedIn = UserStorageService.isCustomerLoggedIn();
+        this.isAdminLoggedIn = UserStorageService.isAdminLoggedIn();
+      }
+    });
+  }
+
+  logout() {
+    UserStorageService.signOut();
+    this.router.navigateByUrl("/");
+  }
 }
+
+
+
 
 
   
